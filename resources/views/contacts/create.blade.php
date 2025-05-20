c1de515336561e4e8974bddd3a9d4494550d4931
deividas <deividas.alekna@stud.svako.lt>
2025-05-20 11:12:29 +0300

    first commit

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.contact')
+@section('content')
+<div class="container">
+<h2>Add Contact</h2>
+<form method="POST" action="{{ route('contacts.store') }}">
+@csrf
+<div>
+<label>Name:</label>
+<input type="text" name="name" required>
+</div>
+<div>
+<label>Phone:</label>
+<input type="text" name="phone" required>
+</div>
+    <div>
+    <label>email:</label>
+    <input type="text" name="email" required>
+    </div>
+<button type="submit">Save</button>
+</form>
+</div>
+@endsection
